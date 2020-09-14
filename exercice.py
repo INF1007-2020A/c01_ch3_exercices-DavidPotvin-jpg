f703127a1f252e6ac37b0424b8d6f38edb8b22a8
DavidPotvin-jpg <david.potvin@polymtl.ca>
2020-09-14 18:15:26 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,7 +10,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return math.cos
+    return angle_degs + angle_minsé
 
 
 def to_degrees(angle_rads: float) -> tuple:
